1b6515d8f4cc15b0a3f4573db971b6d7edeca850
Roger Lu <rogerfrontend@gmail.com>
2019-01-08 05:49:25 -0500

    final commit

--- src/index.js
+++ src/index.js
@@ -1,53 +1,65 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 class ContactCard extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      name: props.name,
-      mobileNum: props.mobileNum,
-      workNum: props.workNum,
-      email: props.email
-    }
-  }
   render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+    const allPerson = this.props.person;
+    const eachPerson = allPerson.map( obj =>
+
+      <div class = "card" >
+        <div class="container">
+          <h4><b>{obj.name}</b></h4>
+          <p>{obj.email}</p>
+          <p>{obj.mobileNum}</p>
+          <p>{obj.workNum}</p>
         </div>
       </div>
+
+    );
+    return(
+      <div>{eachPerson}</div>
     );
   }
 }
 
+
+ContactCard.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  mobileNum: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired
+}
+
 const PERSON = [
-  {name: 'person1',
+  {
+   id: 1,
+   name: 'Rachel',
    email:'person1@email',
    mobileNum:'123-123-1234',
-   workNum: '123-123-1234'
+   //workNum: '123-123-1234'   // its optional
   },
 
-  {name: 'person2',
+  {
+   id: 2,
+   name: 'Jack',
    email:'person2@email',
    mobileNum:'123-123-1234',
-   workNum: '123-123-1234'
+   workNum: '347-555-1234'
   },
 
-  {name: 'person3',
+  {
+   id: 3,
+   name: 'Ana',
    email:'person3@email',
    mobileNum:'123-123-1234',
-   workNum: '123-123-1234'
+   workNum: '646-123-4321'
   }
 ];
 
 ReactDOM.render(
-  <ContactCard />,
+  <ContactCard person={PERSON} />,
+
   document.getElementById('root')
 );
